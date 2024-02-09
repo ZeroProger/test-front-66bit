d504afbe0059998885e73298a21a6ae35dc2287b
Andrey Zhiznevsky <andrey.official.phone@gmail.com>
2024-02-09 18:23:28 +0500

    complete refactor employees-list to useInfiniteQuery

--- src/entities/employee/lib/use-employees.ts
+++ src/entities/employee/lib/use-employees.ts
@@ -1,56 +1,38 @@
-import { useUnit } from 'effector-react'
-import { useState } from 'react'
+import { QueryKeys } from '@/shared/lib/react-query'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { useFilterEmployees } from '.'
-import { EmployeesRequest, fetchEmployees } from '../api'
-import { $page, addEmployeesFn, incrementPageFn, setEmployeesFn, setPageFn } from '../model'
-import { $employees } from './../model/index'
+import { EmployeesRequestWithoutPage, fetchEmployees } from '../api'
 
 export function useEmployees() {
-	const [employees, addEmployees, setEmployees] = useUnit([
-		$employees,
-		addEmployeesFn,
-		setEmployeesFn,
-	])
-	const [page, incrementPage, setPage] = useUnit([$page, incrementPageFn, setPageFn])
-	const [isLoading, setIsLoading] = useState(false)
-	const [hasMoreItems, setHasMoreItems] = useState(true)
-	const { filters } = useFilterEmployees()
 	const count = 10
+	const { filters } = useFilterEmployees()
 
-	const nextEmployees = async () => {
-		setIsLoading(true)
-
-		const { data } = await fetchEmployees({
-			page,
-			count,
-			name: filters.searchTerm,
-			gender: filters.gender,
-			position: filters.position,
-			stack: filters.stack,
-		})
-
-		setHasMoreItems(data.length < 10)
-
-		addEmployees(data)
-		incrementPage()
-
-		setIsLoading(false)
-
-		return data
+	const requestData: EmployeesRequestWithoutPage = {
+		count,
+		name: filters.searchTerm,
+		gender: filters.gender,
+		position: filters.position,
+		stack: filters.stack,
 	}
 
-	const getEmployees = async (params: Omit<EmployeesRequest, 'page' | 'count'>) => {
-		setIsLoading(true)
+	const {
+		data: employees,
+		fetchNextPage,
+		hasNextPage,
+		isFetching,
+		isFetchingNextPage,
+	} = useInfiniteQuery({
+		queryKey: [QueryKeys.employees, requestData],
+		queryFn: ({ pageParam }) => fetchEmployees({ ...requestData, page: pageParam }),
+		initialPageParam: 1,
+		select: (data) => data.pages.flatMap((p) => p.data),
+		getNextPageParam: (lastPage, _, lastPageParam) => {
+			if (lastPage.data.length < 10) {
+				return undefined
+			}
+			return lastPageParam + 1
+		},
+	})
 
-		const { data } = await fetchEmployees({ page: 1, count, ...params })
-
-		setHasMoreItems(data.length < 10)
-
-		setEmployees(data)
-		setPage(2)
-
-		setIsLoading(false)
-	}
-
-	return { employees, isLoading, hasMoreItems, nextEmployees, getEmployees }
+	return { employees, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage }
 }
